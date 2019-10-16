a947601589b0f203ce0b126bb323e42fe833fcb4
moequan <maurice.s93@web.de>
2019-10-16 11:44:09 +0200

    Adding async function for axios.

--- weather.js
+++ weather.js
@@ -11,14 +11,25 @@ const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${ke
     const kindOfWeather = res.data.weather[0].description;
     const temp =res.data.main.temp
 
-    console.log(`In ${city}`)
+    console.log(`In ${city},${res.data.sys.country}`)
     console.log(`Today there is ${weather}`)
     console.log(`It is ${kindOfWeather}`)
     console.log(`We have ${temp} degree`)
    
   }).catch(err =>{
       console.log(err)
-  })
+  });
+
+
+  async function asyncWeather(){
+      try{
+          const response = await axios.get(url);
+          console.log(response);
+      }catch (error){
+          console.log(error)
+      }
+  }
+
 
 
 
